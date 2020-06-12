ff3daae16c4ede1ecfaa4c54b54c1854dd2f11c7
Diep Nguyen <diepniit.nguyenhong@gmail.com>
2020-06-12 16:28:05 +0700

    new repo init

---
+++ testcases/com/wordpress/login/login_01_ValidatedLoginForm.java
@@ -0,0 +1,11 @@
+package com.wordpress.login;
+
+import org.testng.annotations.Test;
+
+public class login_01_ValidatedLoginForm {
+	
+	@Test(enabled = false)
+	public void getCurrentURL() {
+		
+	}
+}
